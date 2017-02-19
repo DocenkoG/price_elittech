8d3bd6b728311471ad77b83d2ad3ae8b8dd11ac2
Docenko G <docn@mail.ru>
2017-02-20 01:50:09 +0300

    Добавлен класс Vendor с заглушками

--- elittech.py
+++ elittech.py
@@ -36,46 +36,62 @@ def config_read():
 
 
 def make_loger():
-   global log
-   logging.config.fileConfig('logging.cfg')
-   log = logging.getLogger('logFile')
-   log.debug('test debug message')
-   log.info( 'test info message')
-   log.warn( 'test warn message')
-   log.error('test error message')
-#  log.critical('test critical message')
-
+    global log
+    logging.config.fileConfig('logging.cfg')
+    log = logging.getLogger('logFile')
+    log.debug('test debug message')
+    log.info( 'test info message')
+    log.warn( 'test warn message')
+    log.error('test error message')
+ #  log.critical('test critical message')
 
 
 
 def main( ):
-   global  myname
-   global  mydir
-   print('myname    =', myname)
-   print('mydir     =', mydir)
+    global  myname
+    global  mydir
+    print('myname    =', myname)
+    print('mydir     =', mydir)
    
-   make_loger()
-   log.debug(myname +', Begin main.')
+    make_loger()
+    log.debug(myname +', Begin main.')
 
- 
-   # Прочитать конфигурацию из файла
-   cfg = config_read()
+    price = Vendor()
+    if  price.download() :
+        price.convert2csv()
+
+
+
+    # Прочитать конфигурацию из файла
+    cfg = config_read()
    
+
+
+
+class Vendor(object):
+    """Объект поставщика, прайс которого мы скачиваем с сайта. """
+    def __init__(self):
+        self.orgName   = myname
+
+
+
+    def download(self):
+        log.debug('begin method Vendor.dowmload')
+        return True
+
+
+
+    def convert2csv(self):
+        log.debug('begin method Vendor.convert2csv')
+
 
 
 
 if __name__ == '__main__':
-   global  myname
-   global  mydir
-   myname   = os.path.basename(os.path.splitext(sys.argv[0])[0])
-   mydir    = os.path.dirname (sys.argv[0])
-   main( )
+    global  myname
+    global  mydir
+    myname   = os.path.basename(os.path.splitext(sys.argv[0])[0])
+    mydir    = os.path.dirname (sys.argv[0])
+    main( )
  
-'''
-elittech_price.init()
-if  elittech_price.downloader():
-    elittech_price.convert2csv()
-
-
 #os.system(r'c:\prices\_scripts\remove_tmp_profiles.cmd')
-'''
